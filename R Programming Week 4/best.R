579679c8ded8f4b5331faa9a98dfaa1d5cc5a5de
Faaez Razeen <razeenfaaez@gmail.com>
2020-02-28 17:37:17 +0530

    Cleaned up best.R

--- R Programming Week 4/best.R
+++ R Programming Week 4/best.R
@@ -9,28 +9,27 @@ best <- function(state, outcome) {
     if(!outcome %in% c("heart attack", "pneumonia", "heart failure"))
         stop('invalid outcome')
     
-    if(outcome == "heart attack"){
-        states <- outcome_df[outcome_df$State == state, ]
-        lowest_death_rate <- sort(as.numeric(states$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack))[1]
-        hospitals <- states[states$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack == lowest_death_rate, ]['Hospital.Name']
-        hospitals <- hospitals[complete.cases(hospitals), ]  
-    }
+    if(outcome == "heart attack")
+        col_name = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
         
-    else if(outcome == "heart failure"){
-        states <- outcome_df[outcome_df$State == state, ]
-        lowest_death_rate <- sort(as.numeric(states$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure))[1]
-        hospitals <- states[states$Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure == lowest_death_rate, ]['Hospital.Name']
-        hospitals <- hospitals[complete.cases(hospitals), ]  
-    }
-    
-    else{
-        states <- outcome_df[outcome_df$State == state, ]
-        lowest_death_rate <- sort(as.numeric(states$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia))[1]
-        hospitals <- states[states$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia == lowest_death_rate, ]['Hospital.Name']
-        hospitals <- hospitals[complete.cases(hospitals), ]      
-    }
+    else if(outcome == "heart failure")
+        col_name = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Failure"
 
-    ## Return hospital name in that state with lowest 30-day death
-    ## rate
+    else
+        col_name = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
+        
+    # as.vector(t(states[col_name])) is needed because indexing using [] returned dataframes which I need to convert to a vector to use.
+    # t() converts a dataframe into a matrix and as.vector() does as the name implies/
+    # as.numeric because sorting needs to be done
+    
+    # WARNING: DOES NOT WORK FOR best("MD", "pneumonia")
+    # Although works with best("TX", "heart attack") and best("TX", "heart failure") 
+    
+    states <- outcome_df[outcome_df$State == state, ]
+    lowest_death_rate <- sort(as.numeric(as.vector(t(states[col_name]))))[1]
+    all_death_rates <- as.vector(t(states[col_name]))
+    hospitals <- states[all_death_rates == lowest_death_rate, ]['Hospital.Name']
+    #hospitals <- hospitals[complete.cases(hospitals), ]['Hospital.Name'] 
+
     hospitals
 }
