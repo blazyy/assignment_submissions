f2cebd167846c0d6abe6f9eb8c87e29d64d38f57
Faaez Razeen <razeenfaaez@gmail.com>
2020-02-28 16:08:29 +0530

    Cleaned up complete.R

--- R Programming Week 2/complete.R
+++ R Programming Week 2/complete.R
@@ -1,19 +1,13 @@
 # Write a function that reads a directory full of files and reports the number of completely observed cases in each data file. The function should return a data frame where the first column is the name of the file and the second column is the number of complete cases. A prototype of this function follows
 
 complete <- function(directory, id = 1:332){
-    
     files <- list.files(directory, full.names = TRUE)
     df <- data.frame() 
-    
     for(i in id){
         opened_file <- read.csv(files[i])
-        opened_file <- opened_file[complete.cases(opened_file), ]
         num_complete_cases <- sum(complete.cases(opened_file)) 
-        vect <- list(i, num_complete_cases)
-        df <- rbind(df, vect)       
+        df <- rbind(df, as.numeric(c(i, num_complete_cases)))       
     }
-    
     colnames(df) <- c('id', 'nobs')
     df
 }
-
