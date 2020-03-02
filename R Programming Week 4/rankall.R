f48a893266c71ad378f12720ae328d2eb3e4dbfb
Faaez Razeen <razeenfaaez@gmail.com>
2020-03-02 14:48:14 +0530

    Finished rankall.R

--- R Programming Week 4/rankall.R
+++ R Programming Week 4/rankall.R
@@ -25,21 +25,20 @@ rankall <- function(outcome, num = "best") {
     return_df <- data.frame('hospital' = character(0), 'state' = character(0), stringsAsFactors = FALSE)
     
     for(name in names(table(outcome_df$State))){
-        all_hospitals_in_state <- outcome_df[outcome_df['State'] == name, ]
         
-        if(num == 'worst')
-            num <- nrow(all_hospitals_in_state)
+        all_hospitals_in_state <- outcome_df[outcome_df['State'] == name, ]
+        ordered_hospitals_in_state <- all_hospitals_in_state[order(all_hospitals_in_state[col_name], all_hospitals_in_state['Hospital.Name']), ]
+        
+        if(num == "worst")
+            hospital <- tail(ordered_hospitals_in_state[, 'Hospital.Name'], 1)
+        else if(num == "best")
+            hospital <- head(ordered_hospitals_in_state[, 'Hospital.Name'], 1)
         else if(num > nrow(all_hospitals_in_state))
             hospital <- NA
-        
-        # Ordering by hospital rating and hospital name, in that order.
-        ordered_hospitals_in_state <- all_hospitals_in_state[order(all_hospitals_in_state[col_name], all_hospitals_in_state['Hospital.Name']), ]
-        # Getting nth hospital from ordered states. This is the rank.
-        hospital <- ordered_hospitals_in_state[num, 'Hospital.Name']
+        else
+            hospital <- ordered_hospitals_in_state[num, 'Hospital.Name']
+
         return_df[nrow(return_df) + 1, ] <- c(hospital, name)
     }
     return_df
 }
-
-tail(rankall("pneumonia", "worst"), 3)
-#tail(rankall("heart failure"), 10)
