951671b4ea8ab86808a1f5a945c6e6c1ea1de1b3
Faaez Razeen <razeenfaaez@gmail.com>
2020-03-02 14:53:27 +0530

    Added comments

--- R Programming Week 4/rankall.R
+++ R Programming Week 4/rankall.R
@@ -6,6 +6,7 @@ rankall <- function(outcome, num = "best") {
     if(!outcome %in% c("heart attack", "pneumonia", "heart failure"))
         stop('invalid outcome')
     
+    # Assign appropriate column names deopending on specified outcome
     if(outcome == "heart attack")
         col_name = "Hospital.30.Day.Death..Mortality..Rates.from.Heart.Attack"
     
@@ -15,20 +16,23 @@ rankall <- function(outcome, num = "best") {
     else
         col_name = "Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia"
     
+    # Convert outcome column to numeric and remove rows with NAs in this column
     outcome_df[, col_name] <- as.numeric(outcome_df[, col_name])
     outcome_df <- outcome_df[!is.na(outcome_df[, col_name]), ]
-    
-    # Check if rank is valid, and use correct numbers depending on "best" or "worst"
-    if(num == 'best')
-        num <- 1
-    
+
+    # Creating empty dataframe which we will fill and return later    
     return_df <- data.frame('hospital' = character(0), 'state' = character(0), stringsAsFactors = FALSE)
     
+    # Looping through each state
     for(name in names(table(outcome_df$State))){
         
+        # Getting rows pertaining to each state
         all_hospitals_in_state <- outcome_df[outcome_df['State'] == name, ]
+        
+        # Ordering rows by col_name first, then by alphabetical order
         ordered_hospitals_in_state <- all_hospitals_in_state[order(all_hospitals_in_state[col_name], all_hospitals_in_state['Hospital.Name']), ]
         
+        # Depending on rank, index different parts of the dataframe
         if(num == "worst")
             hospital <- tail(ordered_hospitals_in_state[, 'Hospital.Name'], 1)
         else if(num == "best")
@@ -38,6 +42,7 @@ rankall <- function(outcome, num = "best") {
         else
             hospital <- ordered_hospitals_in_state[num, 'Hospital.Name']
 
+        # Return dataframe appended with states and their ranked hospitals
         return_df[nrow(return_df) + 1, ] <- c(hospital, name)
     }
     return_df
